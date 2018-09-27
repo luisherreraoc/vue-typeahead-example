31ff267a31dda7a28a2bd05d4db84c52f3aa426d
Luis Acedo <lacedo@oceano.com>
2018-09-27 12:53:04 +0200

    reset del proyecto

---
+++ src/components/Typeahead.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Typeahead',
+  props: {
+
+  },
+  data () {
+    return {
+
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
